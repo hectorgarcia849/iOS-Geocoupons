4ff63ec6d94a05ec9299ad7da6b55f7983bfd2e2
Hector Garcia <hectorgarcia@imac.local>
2016-12-10 17:52:14 -0500

    Initial Commit

---
+++ geocoupons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  geocoupons
+//
+//  Created by Hector Garcia on 2016-12-10.
+//  Copyright © 2016 Hector Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ geocoupons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  geocoupons
+//
+//  Created by Hector Garcia on 2016-12-10.
+//  Copyright © 2016 Hector Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
